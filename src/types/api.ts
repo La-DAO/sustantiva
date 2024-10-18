6ad97c52e974c33d09146e03bbefcdfa69f5447d
Mel <angel.melendez.c@gmail.com>
2024-10-18 12:46:03 -0700

    create loan apps and display in ui when new is created

--- src/types/api.ts
+++ src/types/api.ts
@@ -26,9 +26,31 @@ export type CreatePassportProfileData = {
   totalLimit?: number
 }
 
+export type CreateLoanApplicationData = {
+  amount: number
+  availableCreditLine: number
+  status?: string
+  xocScore: number
+  builderScore: number
+  nominationsReceived: number
+  followers: number
+  walletId: string
+  applicantId: number
+  creditLineId: number
+  reviewedById?: number
+}
+
 export interface PassportProfileExtended extends CreatePassportProfileData {
+  id?: number
   creditLine?: CreditLine
   loans: Loan[]
   loanApplications: LoanApplication[]
   underwriterProfile?: UnderwriterProfile
+}
+
+export interface LoanApplicationExtended extends CreateLoanApplicationData {
+  id?: number
+  creditLine?: CreditLine
+  applicant: PassportProfileExtended
+  reviewedBy?: UnderwriterProfile
 }
